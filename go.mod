b09e79d204a51d451bf2bb850e8b0a99be3f8313
Anas Mohamed <14171441+amohamed11@users.noreply.github.com>
2022-03-19 02:34:11 -0600

    feat: list/view/create notes & render with goldmark done

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/gin-gonic/gin v1.7.7 // indirect
 	github.com/mattn/go-sqlite3 v1.14.12 // indirect
+	github.com/yuin/goldmark v1.4.10 // indirect
 	gorm.io/driver/sqlite v1.3.1 // indirect
 	gorm.io/gorm v1.23.2 // indirect
 )
--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"bytes"
+	"html/template"
 	"net/http"
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/renderer/html"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 type Note struct {
 	gorm.Model
+	Title   string
 	Content string
 	Tags    []Tag `gorm:"many2many:note_tags;"`
 }
@@ -21,6 +26,7 @@ type Tag struct {
 }
 
 type NoteForm struct {
+	Title   string `form:"title"`
 	Content string `form:"content"`
 	Tags    string `form:"tags"`
 }
@@ -34,19 +40,42 @@ func main() {
 	// Migrate the schema
 	db.AutoMigrate(&Note{})
 
+	// Setup goldmark
+	md := goldmark.New(
+		goldmark.WithRendererOptions(
+			html.WithHardWraps(),
+			html.WithXHTML(),
+		),
+	)
+
 	// Setup server
 	router := gin.Default()
 	router.SetTrustedProxies([]string{"localhost"})
-	router.LoadHTMLGlob("templates/*")
+	router.LoadHTMLGlob("templates/**/*.tmpl")
 	router.Static("/assets", "./assets")
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"title": "Kitab",
+		var notes []Note
+		result := db.Find(&notes)
+
+		if result.Error == nil {
+			c.HTML(http.StatusOK, "home/index.tmpl", gin.H{
+				"count": len(notes),
+				"notes": notes,
+			})
+			return
+		}
+		c.HTML(http.StatusInternalServerError, "shared/error.tmpl", gin.H{
+			"msg":   "Something went wrong!",
+			"error": result.Error,
 		})
 	})
 
-	router.POST("/note", func(c *gin.Context) {
+	router.GET("/notes/new", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "notes/new.tmpl", gin.H{})
+	})
+
+	router.POST("/notes/new", func(c *gin.Context) {
 		var form NoteForm
 		if err := c.ShouldBind(&form); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -60,26 +89,47 @@ func main() {
 			db.FirstOrCreate(&tag, Tag{Name: s})
 			tags = append(tags, tag)
 		}
-		note := Note{Content: form.Content, Tags: tags}
+		note := Note{Title: form.Title, Content: form.Content, Tags: tags}
 		result := db.Create(&note)
 
-		if result.RowsAffected > 0 {
-			c.JSON(http.StatusCreated, gin.H{"body": note})
+		if result.RowsAffected > 0 && result.Error == nil {
+			var buf bytes.Buffer
+			if err := md.Convert([]byte(note.Content), &buf); err != nil {
+				panic(err)
+			}
+			c.HTML(http.StatusCreated, "notes/view.tmpl", gin.H{
+				"note":        note,
+				"contentHTML": template.HTML(buf.String()),
+			})
 			return
 		}
 
-		c.JSON(http.StatusBadRequest, gin.H{"error": result.Error})
+		c.HTML(http.StatusInternalServerError, "shared/error.tmpl", gin.H{
+			"msg":   "Something went wrong!",
+			"error": result.Error,
+		})
 	})
 
-	router.GET("/note", func(c *gin.Context) {
-		var notes []Note
-		result := db.Find(&notes)
+	router.GET("/notes/:id", func(c *gin.Context) {
+		var note Note
+		id := c.Param("id")
+		result := db.Preload("Tags").Find(&note, id)
 
-		if result.RowsAffected > 1 {
-			c.JSON(http.StatusOK, gin.H{"body": notes})
+		if result.Error == nil {
+			var buf bytes.Buffer
+			if err := md.Convert([]byte(note.Content), &buf); err != nil {
+				panic(err)
+			}
+			c.HTML(http.StatusOK, "notes/view.tmpl", gin.H{
+				"note":        note,
+				"contentHTML": template.HTML(buf.String()),
+			})
+			return
 		}
-
-		c.JSON(http.StatusOK, gin.H{"body": "No notes created yet!"})
+		c.HTML(http.StatusInternalServerError, "shared/error.tmpl", gin.H{
+			"msg":   "Something went wrong!",
+			"error": result.Error,
+		})
 	})
 
 	// Listen and serve on 0.0.0.0:8080
