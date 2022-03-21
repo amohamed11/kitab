ea8b3e4912fad3f22f5556cfd5d19748f6d6678f
Anas Mohamed <14171441+amohamed11@users.noreply.github.com>
2022-03-21 00:40:19 -0600

    feat: added ability to search notes by title & content

--- main.go
+++ main.go
@@ -13,11 +13,22 @@ import (
 	"gorm.io/gorm"
 )
 
+type Server struct {
+	DB *gorm.DB
+	MD goldmark.Markdown
+}
+
 type Note struct {
 	gorm.Model
 	Title   string
 	Content string
 	Tags    []Tag `gorm:"many2many:note_tags;"`
+}
+
+type NoteSearch struct {
+	ID        int
+	Noterowid int
+	Content   string
 }
 
 type Tag struct {
@@ -37,8 +48,9 @@ func main() {
 	if err != nil {
 		panic("failed to connect database")
 	}
-	// Migrate the schema
-	db.AutoMigrate(&Note{})
+	// Migrate the schema & setup tables
+	db.AutoMigrate(&Note{}, &Tag{})
+	db.Exec("CREATE VIRTUAL TABLE IF NOT EXISTS notesearch using fts5(noteid, title, content)")
 
 	// Setup goldmark
 	md := goldmark.New(
@@ -48,90 +60,137 @@ func main() {
 		),
 	)
 
-	// Setup server
+	// Setup server object
+	server := Server{DB: db, MD: md}
+
+	// Setup router
 	router := gin.Default()
 	router.SetTrustedProxies([]string{"localhost"})
 	router.LoadHTMLGlob("templates/**/*.tmpl")
 	router.Static("/assets", "./assets")
 
-	router.GET("/", func(c *gin.Context) {
-		var notes []Note
-		result := db.Preload("Tags").Select("id", "title").Find(&notes)
-
-		if result.Error == nil {
-			c.HTML(http.StatusOK, "home/index.tmpl", gin.H{
-				"count": len(notes),
-				"notes": notes,
-			})
-			return
-		}
-		c.HTML(http.StatusInternalServerError, "shared/error.tmpl", gin.H{
-			"msg":   "Something went wrong!",
-			"error": result.Error,
-		})
-	})
-
+	router.GET("/", server.Index)
+	router.GET("/notes/:id", server.GetNoteById)
 	router.GET("/notes/new", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "notes/new.tmpl", gin.H{})
 	})
-
-	router.POST("/notes/new", func(c *gin.Context) {
-		var form NoteForm
-		if err := c.ShouldBind(&form); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		var tags []Tag
-		splitTags := strings.Split(form.Tags, " ")
-		for _, s := range splitTags {
-			var tag Tag
-			db.FirstOrCreate(&tag, Tag{Name: s})
-			tags = append(tags, tag)
-		}
-		note := Note{Title: form.Title, Content: form.Content, Tags: tags}
-		result := db.Create(&note)
-
-		if result.RowsAffected > 0 && result.Error == nil {
-			var buf bytes.Buffer
-			if err := md.Convert([]byte(note.Content), &buf); err != nil {
-				panic(err)
-			}
-			c.HTML(http.StatusCreated, "notes/view.tmpl", gin.H{
-				"note":        note,
-				"contentHTML": template.HTML(buf.String()),
-			})
-			return
-		}
-
-		c.HTML(http.StatusInternalServerError, "shared/error.tmpl", gin.H{
-			"msg":   "Something went wrong!",
-			"error": result.Error,
-		})
-	})
-
-	router.GET("/notes/:id", func(c *gin.Context) {
-		var note Note
-		id := c.Param("id")
-		result := db.Preload("Tags").Find(&note, id)
-
-		if result.Error == nil {
-			var buf bytes.Buffer
-			if err := md.Convert([]byte(note.Content), &buf); err != nil {
-				panic(err)
-			}
-			c.HTML(http.StatusOK, "notes/view.tmpl", gin.H{
-				"note":        note,
-				"contentHTML": template.HTML(buf.String()),
-			})
-			return
-		}
-		c.HTML(http.StatusInternalServerError, "shared/error.tmpl", gin.H{
-			"msg":   "Something went wrong!",
-			"error": result.Error,
-		})
-	})
+	router.POST("/notes/new", server.NewNote)
+	router.POST("/notes/search", server.SearchNote)
 
 	// Listen and serve on 0.0.0.0:8080
 	router.Run(":8080")
+}
+
+func (s *Server) Index(c *gin.Context) {
+	var notes []Note
+	result := s.DB.Preload("Tags").Select("id", "title").Find(&notes)
+
+	if result.Error == nil {
+		c.HTML(http.StatusOK, "home/index.tmpl", gin.H{
+			"count": len(notes),
+			"notes": notes,
+		})
+		return
+	}
+
+	c.HTML(http.StatusInternalServerError, "shared/error.tmpl", gin.H{
+		"msg":   "Something went wrong!",
+		"error": result.Error,
+	})
+}
+
+func (s *Server) GetNoteById(c *gin.Context) {
+	var note Note
+	id := c.Param("id")
+	result := s.DB.Preload("Tags").Find(&note, id)
+
+	if result.Error == nil {
+		var buf bytes.Buffer
+		if err := s.MD.Convert([]byte(note.Content), &buf); err != nil {
+			panic(err)
+		}
+		c.HTML(http.StatusOK, "notes/view.tmpl", gin.H{
+			"note":        note,
+			"contentHTML": template.HTML(buf.String()),
+		})
+		return
+	}
+	c.HTML(http.StatusInternalServerError, "shared/error.tmpl", gin.H{
+		"msg":   "Something went wrong!",
+		"error": result.Error,
+	})
+}
+
+func (s *Server) NewNote(c *gin.Context) {
+	var form NoteForm
+	if err := c.ShouldBind(&form); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var tags []Tag
+	splitTags := strings.Split(form.Tags, " ")
+	for _, tagString := range splitTags {
+		var tag Tag
+		s.DB.FirstOrCreate(&tag, Tag{Name: tagString})
+		tags = append(tags, tag)
+	}
+	note := Note{Title: form.Title, Content: form.Content, Tags: tags}
+	result := s.DB.Create(&note)
+
+	if result.RowsAffected > 0 && result.Error == nil {
+		// Save to virtual table for easier searching of content
+		s.DB.Exec(
+			"INSERT INTO notesearch VALUES(?, ?, ?)",
+			note.ID, note.Title, note.Content,
+		)
+		// Render markdown
+		var buf bytes.Buffer
+		if err := s.MD.Convert([]byte(note.Content), &buf); err != nil {
+			panic(err)
+		}
+		c.HTML(http.StatusCreated, "notes/view.tmpl", gin.H{
+			"note":        note,
+			"contentHTML": template.HTML(buf.String()),
+		})
+		return
+	}
+
+	c.HTML(http.StatusInternalServerError, "shared/error.tmpl", gin.H{
+		"msg":   "Something went wrong!",
+		"error": result.Error,
+	})
+}
+
+func (s *Server) SearchNote(c *gin.Context) {
+	type SearchForm struct {
+		Query string `form:"query" binding:"required"`
+	}
+	var searchForm SearchForm
+	if err := c.ShouldBind(&searchForm); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var notes []Note
+	searchResults := s.DB.Raw(`
+		SELECT *
+		FROM notesearch
+		INNER JOIN notes ON notesearch.noteid = notes.id
+		WHERE notesearch MATCH ?`,
+		searchForm.Query,
+	).Scan(&notes)
+
+	if searchResults.Error == nil {
+		c.HTML(http.StatusOK, "home/index.tmpl", gin.H{
+			"count": len(notes),
+			"notes": notes,
+		})
+		return
+	}
+
+	c.HTML(http.StatusInternalServerError, "shared/error.tmpl", gin.H{
+		"msg":   "Something went wrong!",
+		"error": searchResults.Error,
+	})
 }
